9a9275ff8f8c4e4badeae3c25d9121ff82d328d6
unknown <icomarce69@gmail.com>
2023-02-20 20:28:27 -0300

    configuracion cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_gjeXH9FOo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2cfbfn9uu9u9f7nb6e294gpnk4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
